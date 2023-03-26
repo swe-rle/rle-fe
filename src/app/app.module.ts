32aef672857eb9bc432d7c0c55468a096a725dd1
mohsayeed <cs19btech11004@iith.ac.in>
2023-03-26 08:38:24 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
-import { NgxUiLoaderModule } from 'ngx-ui-loader';
+import { NgxUiLoaderConfig, NgxUiLoaderHttpModule, NgxUiLoaderModule, NgxUiLoaderRouterConfig, NgxUiLoaderRouterModule, PB_DIRECTION, POSITION, SPINNER } from 'ngx-ui-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { FeaturesModule } from './features/features.module';
 
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  bgsColor: 'red',
+  bgsType: SPINNER.threeStrings,
+  blur: 25,
+  fastFadeOut: true,
+  fgsColor: 'red',
+  fgsType: SPINNER.squareJellyBox,
+  pbColor: 'red',
+  pbThickness: 5,
+  hasProgressBar: true,
+};
 @NgModule({
   declarations: [
     AppComponent
@@ -19,7 +30,8 @@ import { FeaturesModule } from './features/features.module';
     LazyLoadImageModule,
     FeaturesModule,
     CoreModule,
-    NgxUiLoaderModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
+    NgxUiLoaderHttpModule.forRoot({ showForeground: true }),    
     HttpClientModule
   ],
   providers: [],
