5dc4ea32f5e60c25039581799838d9755484ad95
mohsayeed <cs19btech11004@iith.ac.in>
2023-03-30 13:24:13 +0530

    added carasourl

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { GalleryComponent, GalleryConfig, GalleryItem, ImageItem, LoadingStrategy, SlidingDirection, ThumbnailsPosition, ThumbnailsView } from 'ng-gallery';
+import { catchError, map, Observable } from 'rxjs';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
+  config: GalleryConfig | undefined;
+  images!: GalleryItem[];
+ngOnInit() {
+  this.images = [
+    new ImageItem({ src: 'assets/img/slider/compiler-1.jpg'}),
+    new ImageItem({ src: 'assets/img/slider/cse-2.jpg'}),
+    new ImageItem({ src: 'assets/img/slider/cse-1.jpg'}),
+    new ImageItem({ src: 'assets/img/slider/faculty-2.jpg'}),
+    new ImageItem({ src: 'assets/img/slider/faculty-1.jpg'}),
+    new ImageItem({ src: 'assets/img/slider/iith.jpg'}),
+
+    // ... more items
+  ];
+  this.config = {
+   thumb:false,
+   dots: true,
+    loadingStrategy: LoadingStrategy.Preload,
+    slidingDirection: SlidingDirection.Horizontal,
+
+  };
+}
 
 }
