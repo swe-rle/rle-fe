3d19736b743f4cb3b283fd2a665e2b904dc36406
B Amogh <cs19btech11031@iith.ac.in>
2023-04-28 16:02:50 +0530

    added admin ui

--- src/app/admin/admin-layout/admin-footer/admin-footer.component.ts
+++ src/app/admin/admin-layout/admin-footer/admin-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-admin-footer',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-footer.component.scss']
 })
 export class AdminFooterComponent {
-
+  @Input() footerDetails: any;
 }
