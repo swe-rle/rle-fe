ebd47ddd4f3c3a904d9e2ab1a9cd04a67a867a4e
mohsayeed <cs19btech11004@iith.ac.in>
2023-04-28 16:01:02 +0530

    changed routing

--- src/app/services/gallery/gallery.service.ts
+++ src/app/services/gallery/gallery.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from 'src/app/core/service/http/http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GalleryService {
 
-  constructor() { }
+  constructor(private httpService:HttpService) { }
 }
